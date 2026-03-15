20a749e4dd47390efcfb9caeb461c6023e2a5a8c
SLCoder1 <sathvik.cjhs.965@gmail.com>
2026-03-14 23:01:06 -0700

    Nearest Function method works

--- main.py
+++ main.py
@@ -12,13 +12,24 @@ def loadData(filename):
             data.append(row)
     return data
 
-def euclideanDistance(row1, row2, feature_set):
+def euclideanDistance(row1, row2, featureSet):
     total = 0.0
-    for feature in feature_set:
+    for feature in featureSet:
         total += (row1[feature] - row2[feature]) ** 2
     return math.sqrt(total)
 
+def nearestNeighbor(data, index, featureSet):
+    bestDistance = float('inf')
+    bestLabel = None
 
+    for i in range(len(data)):
+        if i == index:
+            continue
+        distance = euclideanDistance(data[index], data[i], featureSet)
+        if distance < bestDistance:
+            bestDistance = distance
+            bestLabel = data[i][0]
+    return bestLabel
 
 
 def main():
@@ -31,6 +42,10 @@ def main():
 
     if nInstances >= 2:
         print("Distance test with features[1, 2]:", euclideanDistance(data[0], data[1], [1,2]))
+
+    predicted = nearestNeighbor(data, 0, [1,2])
+    actual = data[0][0]
+    print(f"Predicted label: {predicted}, Actual label: {actual}")
 
 if __name__ == "__main__":
     main()
