aeb8844ac207f869bfba719c123fde3bc402f1bc
SLCoder1 <sathvik.cjhs.965@gmail.com>
2026-03-14 22:02:35 -0700

    Created Euclidean Distance function

--- main.py
+++ main.py
@@ -12,6 +12,15 @@ def loadData(filename):
             data.append(row)
     return data
 
+def euclideanDistance(row1, row2, feature_set):
+    total = 0.0
+    for feature in feature_set:
+        total += (row1[feature] - row2[feature]) ** 2
+    return math.sqrt(total)
+
+
+
+
 def main():
     filename = "SanityCheckDataSet__2.txt"
     data = loadData(filename)
@@ -19,6 +28,9 @@ def main():
     nFeatures = len(data[0]) - 1
     print(f"Number of instances: {nInstances}")
     print(f"Number of features: {nFeatures}")
+
+    if nInstances >= 2:
+        print("Distance test with features[1, 2]:", euclideanDistance(data[0], data[1], [1,2]))
 
 if __name__ == "__main__":
     main()
